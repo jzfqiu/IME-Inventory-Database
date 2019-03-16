bb2ee304236ffc45a7b7492d20ac4baf409097d0
Jeff Qiu <zqiu@uchicago.edu>
2019-03-16 17:49:29 -0500

    added mongodb CRUD operations in db.py

--- web/__init__.py
+++ web/__init__.py
@@ -3,9 +3,12 @@ from web import config
 
 
 def create_app():
-    # create and configure the app
+    # create and configure the app from config.py
     app = Flask(__name__)
     app.config.from_object(config.Config)
+
+    from . import db
+    db.init_db()
 
     # a simple page that says hello
     @app.route('/')
--- web/db.py
+++ web/db.py
@@ -1,15 +1,66 @@
+"""
+All pymongo operations are LAZY
+"""
+
 from flask import current_app, g
+import pymongo
+from bson import BSON
 
 
-def get_db():
+def init_db(db_name):
+    """
+    Initialize MongoDB database into request's global instance
+
+    :param db_name: string, name of database
+    :return: pymongo Database object
+    """
     if 'db' not in g:
         # connect to mongo db
-        client = MongoClient(current_app.config['MONGO_HOST'],
-                             current_app.config['MONGO_PORT'])
-
+        client = pymongo.MongoClient(current_app.config['MONGO_HOST'],
+                                     current_app.config['MONGO_PORT'])
+        g.db = client[db_name]
     return g.db
 
 
-def close_db():
-    # kick db out of global request instance
-    db = g.pop('db', None)
+def insert_one(collection, document):
+    """
+    Insert a single document into given collection
+
+    :param collection: pymongo collection object
+    :param document: dict, key-value pair of data
+    :return: id of inserted entry
+    """
+    return collection.insert_one(document).inserted_id
+
+
+
+def find_all(collection, f=None):
+    """
+    Query a collection
+
+    :param f: dict, filter for query, can be nested dict
+    :return: pymongo cursor object, index like python dict
+    """
+
+    return collection.find(BSON.encode(f))
+
+
+
+def delete_all(collection, f):
+    """
+    :return: number of documents deleted
+    """
+    return collection.delete_many(BSON.encode(f)).deleted_count
+
+
+
+def update_all(collection, f, update):
+    """
+    :return: # of documents found and # of documents updated
+    """
+    result = collection.update_many(BSON.encode(f), BSON.encode(update))
+    return result.matched_count, result.modified_count
+
+
+
+
