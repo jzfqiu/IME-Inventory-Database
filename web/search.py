54fb10eb4b669eea22ee5c9af7333b2898330b59
Jeff Qiu <zqiu@uchicago.edu>
2019-04-03 22:13:01 -0500

    added template and backend for document page

--- web/search.py
+++ web/search.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash
 from web import db
+from bson.objectid import ObjectId
 
 
 search_bp = Blueprint('search', __name__)
@@ -27,9 +28,32 @@ def search(page):
         batch = collection.find(None)
 
     for item in batch:
-        object_id = item.pop('_id')
-
-        results.append({'id': object_id, 'data': item})
+        results.append(process_item(item, 5))
 
     return render_template('search.html', results=results)
 
+
+@search_bp.route('/doc/<obj_id>')
+def document(obj_id):
+    collection = db.get_db()['inventory']
+    item = collection.find_one({'_id': ObjectId(obj_id)})
+    result = process_item(item)
+    return render_template('document.html', result=result)
+
+
+def process_item(item, n=None):
+    """
+    :param item: a document (dict) returned by find_one() or batch iteration
+    :param n: number of entry needed in detailed ['data']
+    :return: another dict with _id and name seperated from other data
+    """
+    try:
+        object_id = item.pop('_id')
+        name = item.pop('name')
+        if n:
+            data = list(item.items())[:n]
+        else:
+            data = list(item.items())
+    except KeyError:
+        raise
+    return {'id': object_id, 'name': name, 'data': data}
