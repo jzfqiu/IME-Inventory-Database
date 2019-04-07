793a1e1fca049180b754fa4ed3c9c17bda2cee6d
Jeff Qiu <zqiu@uchicago.edu>
2019-04-07 01:00:48 -0500

    added script to prevent empty submission

--- web/static/script.js
+++ web/static/script.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
+
+
     var fieldCounts = 3
 
+
+    // control addField button behavior in /edit/..
     $("#addField").click(function(){
         fieldCounts++;
 
         var new_field = '<div id="editField' + fieldCounts +
-        '"><input type="text" name="key' + fieldCounts +
-        '">: <input type="text" name="value'+ fieldCounts +
-        '"> <button type="button" class="delFieldButton" id="delField' + fieldCounts +
+        '"><input class="field" type="text" name="key' + fieldCounts +
+        '">: <input class="field" type="text" name="value'+ fieldCounts +
+        '"> <button class="delFieldButton" type="button" id="delField' + fieldCounts +
         '">x</button><br></div>'
 
         $("#editFields").append(new_field);
     });
+
 
     // 'on' method to make sure event handler is attached to dynamically created elements
     // method attached to '#editField' because it's the closest static parent element
@@ -19,6 +24,17 @@ $(document).ready(function(){
         var del_field_id = 'editField'+$(this).attr('id').charAt(8);
         $('#'+del_field_id).remove();
         fieldCounts--;
+    });
+
+
+    $('#submitField').click(function(e){
+        $('.field').each(function(){
+            if ($(this).val() == '') {
+                alert('Field cannot be empty!');
+                e.preventDefault();
+                return false;
+            };
+        });
     });
 
 
