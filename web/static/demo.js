b3418cf0dcb5c679abe3a6654ae5c43f6396ef98
Jeff Qiu <zqiu@uchicago.edu>
2019-10-09 11:02:45 -0500

    fixed category dropdown hover problem

--- web/static/demo.js
+++ web/static/demo.js
@@ -2,13 +2,32 @@
 
 $(document).ready(function(){
 
+
+    // $('.catNav__cat')
+    //     .mouseover( function () {
+    //         let hover_nav = $(this).attr('id').charAt(3);
+    //         $('#catDropdown'+hover_nav).css('display', 'flex');
+    //     });
+    //
+    // $('.catDropdown')
+    //     .mouseout(function () {
+    //         let hover_nav = $(this).attr('id').charAt(11);
+    //         $('#catDropdown'+hover_nav).css('display', 'none');
+    //     });
+    let hover_nav = -1;
+
     $('.catNav__cat, .catDropdown').hover(
         function () {
-            $('.catDropdown').css('display', 'flex');
+            hover_nav = $(this).attr('id').charAt(3);
+            $('#cat'+hover_nav).addClass('catNav__cat--hover');
+            $('#cat'+hover_nav+'Dropdown').css('display', 'flex');
         },
         function () {
-            $('.catDropdown').css('display', 'none');
-        }
-    )
+            $('#cat'+hover_nav).removeClass('catNav__cat--hover');
+            $('#cat'+hover_nav+'Dropdown').css('display', 'none');
+        });
+
+
+
 
 });
