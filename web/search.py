a20e650a5ada51da6c5f6c2141b97351d52a4c02
Jeff Qiu <zqiu@uchicago.edu>
2020-02-01 16:39:06 -0600

    organized test data jsons

--- web/search.py
+++ web/search.py
@@ -6,9 +6,9 @@ search_bp = Blueprint('search', __name__)
 
 @search_bp.route('/', methods=('GET', 'POST'))
 def search():
-    with open('test_cats.json') as cats:
+    with open('test_data/test_cats.json') as cats:
         cats_data = list(json.load(cats).values())
-    with open('test_data_v2.json') as demos:
+    with open('test_data/test_data_v2.json') as demos:
         demo_data = list(json.load(demos).values())[0]
     return render_template('search.html', cats=cats_data, demos=demo_data)
 
