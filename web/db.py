9451d79e0c5889c98417af5ae823bb104396cc51
Jeff Qiu <zqiu@uchicago.edu>
2020-02-01 22:14:40 -0600

    preliminary search function

---
+++ web/db.py
@@ -0,0 +1,47 @@
+"""
+All pymongo operations are LAZY
+"""
+
+from flask import current_app, g
+import pymongo
+
+
+def get_db(db_name='db'):
+    """
+    Initialize MongoDB database into request's global instance
+    :param db_name: string, name of database
+    :return: pymongo Database object
+    """
+    if 'db' not in g:
+        # connect to mongo db
+        mongo_host = current_app.config['MONGO_HOST']
+        mongo_port = int(current_app.config['MONGO_PORT'])
+        mongo_username = current_app.config['MONGO_INITDB_ROOT_USERNAME']
+        mongo_password = current_app.config['MONGO_INITDB_ROOT_PASSWORD']
+        client = pymongo.MongoClient(host=mongo_host,
+                                     port=mongo_port,
+                                     username=mongo_username,
+                                     password=mongo_password,
+                                     serverSelectionTimeoutMS=1000)
+
+        g.db = client[db_name]
+    return g.db
+
+
+def process_item(item, n=None):
+    """
+    Helper function for cleaning searched result
+    :param item: a document (dict) returned by find_one() or batch iteration
+    :param n: number of entry needed in detailed ['data']
+    :return: another dict with _id and name separated from other data
+    """
+    try:
+        object_id = item.pop('_id')
+        name = item.pop('name')
+        if n:
+            data = list(item.items())[:n]
+        else:
+            data = list(item.items())
+    except KeyError:
+        raise
+    return {'id': object_id, 'name': name, 'data': data}
--- web/search.py
+++ web/search.py
@@ -1,17 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session
 import json
+import math
+from web import db
 
+RESULT_PER_PAGE = 15
 search_bp = Blueprint('search', __name__)
 
 
 @search_bp.route('/', methods=('GET', 'POST'))
-def search():
+@search_bp.route('/page/<page_number>', methods=('GET', 'POST'))
+def search(page_number='1'):
+    collection = db.get_db()['inventory']
+    if request.method == 'POST' and request.form['keywords']:
+        keywords = request.form['keywords']
+        batch = collection.find({"$text": {"$search": keywords}})
+        batch_cnt = collection.count({"$text": {"$search": keywords}})
+    else:
+        batch = collection.find(None).limit(RESULT_PER_PAGE).skip(
+            (int(page_number)-1)*RESULT_PER_PAGE)
+        batch_cnt = collection.count(None)
     with open('test_data/test_cats.json') as cats:
         cats_data = list(json.load(cats).values())
-    with open('test_data/test_data_v2.json') as demos:
-        demo_data = list(json.load(demos).values())[0]
-    return render_template('search.html', cats=cats_data, demos=demo_data)
+    return render_template('search.html',
+                           cats=cats_data,
+                           data=batch,
+                           page_cnt=batch_cnt,
+                           pages=range(math.ceil(batch_cnt/RESULT_PER_PAGE)))
 
-# Add location inforamtion
-
+# Add location information
 # color coding by campus
