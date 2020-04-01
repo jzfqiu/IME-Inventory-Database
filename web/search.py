0a144c4533f33416eadd56bef4fdd468470d2a13
Jeff Qiu <zqiu@uchicago.edu>
2020-04-01 00:44:48 -0500

    added category options fetching functions

--- web/search.py
+++ web/search.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, render_template, request, session, current_app, redirect, url_for
+"""search logic"""
+
 import json
 import math
-from web import db
-from bson.objectid import ObjectId
 from hashlib import blake2b
+from bson.objectid import ObjectId
+
+from flask import Blueprint, render_template, request, session, current_app, redirect, url_for
+from web import db
+
 
 RESULT_PER_PAGE = 15
 search_bp = Blueprint('search', __name__)
@@ -12,19 +16,35 @@ search_bp = Blueprint('search', __name__)
 # search: return search page skeleton
 # search.js: send request for result with empty criteria
 # fetch: fetch results from db, return a populated html page
-# search.js: asyn receive html and insert into search.html
+# search.js: async receive html and insert into search.html
 
 
-def dprint(s):
-    print(s, flush=True)
+def dprint(string):
+    """Print to docker-compose log"""
+    print(string, flush=True)
 
 
 def get_logged_in_user():
+    """Get logged in user from session"""
     return session.get('username', None)
+
+
+def flatten_dict(d):
+    """Given nested, single-key dict, return a list of all keys and value"""
+    res = []
+    while isinstance(d, dict):
+        k = list(d.keys())[0]
+        res.append(k)
+        d = d[k]
+    res.append(d)
+    return res
 
 
 @search_bp.route('/', methods=('GET', 'POST'))
 def search():
+    """
+    Front page: render sidebar and top
+    """
     with open('test_data/test_cats.json') as cats:
         cats_data = list(json.load(cats).values())
     return render_template('search.html',
@@ -48,13 +68,12 @@ def fetch_page(page_number):
 
 @search_bp.route('/fetch/login', methods=['POST'])
 def fetch_login():
-    user = list(db.get_db()['user'].find({'email': request.form['email']}))[0]
+    user_requested = list(db.get_db()['user'].find({'email': request.form['email']}))[0]
     hashed_pwd = blake2b(str.encode(request.form['password']), digest_size=10)
-    if user is not None and user['password']==hashed_pwd.hexdigest():
-        session['username'] = [user['username'], user['name']]
+    if user_requested is not None and user_requested['password']==hashed_pwd.hexdigest():
+        session['username'] = [user_requested['username'], user_requested['name']]
         return json.dumps({'success': True})
-    else:
-        return json.dumps({'success': False})
+    return json.dumps({'success': False})
 
 
 @search_bp.route('/fetch/logout')
@@ -65,15 +84,15 @@ def fetch_logout():
 
 @search_bp.route('/user/<username>')
 def user(username):
-    user = db.get_user_by_username(username)
-    if user is not None:
+    user_requested = db.get_user_by_username(username)
+    if user_requested is not None:
         for k in ['_id', 'password']:
-            user.pop(k)
-        user_equipments = db.get_equipments(user['equipments'])
+            user_requested.pop(k)
+        user_equipments = db.get_equipments(user_requested['equipments'])
         user_equipments = [{'name': e['name'], 'id': str(e['_id'])} for e in user_equipments]
-        is_manager = get_logged_in_user() and user['username']==get_logged_in_user()[0]
-        return render_template('user.html', 
-                                user=user,
+        is_manager = get_logged_in_user() and user_requested['username'] == get_logged_in_user()[0]
+        return render_template('user.html',
+                                user=user_requested,
                                 logged_in_user=get_logged_in_user(),
                                 equipments=user_equipments,
                                 is_manager=is_manager)
@@ -85,30 +104,43 @@ def user(username):
 def equipment(_id):
     equipment = db.get_one_equipment(ObjectId(_id))
     _, cat, _ = db.unroll_cat(equipment['category'], True)
+    campus_str = " - ".join([[key, value] for (key, value) in equipment['campus'].items()][0])
     return render_template('equipment.html',
                            equipment=equipment,
                            cat=cat,
+                           campus=campus_str,
                            GOOGLE_MAP_API_KEY=current_app.config['GOOGLE_MAP_API_KEY'],
                            logged_in_user=get_logged_in_user())
 
 
 @search_bp.route('/equipment/edit/<_id>')
 def edit_equipment(_id):
-    equipment = db.get_one_equipment(ObjectId(_id))
-    is_manager = get_logged_in_user()==equipment['user']
+    equipment_requested = db.get_one_equipment(ObjectId(_id))
+    is_manager = get_logged_in_user() == equipment_requested['user']
+    existing_cat = flatten_dict(equipment_requested['category'])
     return render_template('edit.html',
-                            equipment=equipment,
+                            equipment=equipment_requested,
+                            existing_cat=existing_cat,
                             is_manager=is_manager,
+                            GOOGLE_MAP_API_KEY=current_app.config['GOOGLE_MAP_API_KEY'],
                             logged_in_user=get_logged_in_user())
-    
+
 
 @search_bp.route('/about')
 def about():
     return render_template('about.html', logged_in_user=get_logged_in_user())
 
 
+@search_bp.route('/fetch/edit/cat', methods=['POST'])
+def fetch_cat():
+    with open('test_data/test_cats_v2.json') as cats:
+        cats_data = json.load(cats)
+    data = request.get_json()
+    cat = data.get('cat', None)
+    bucket = data.get('bucket', None)
+    if bucket is None:
+        return json.dumps(list(cats_data[cat].keys()))
+    return json.dumps(cats_data[cat][bucket])
 
 
-
-# TODO: Add location information
 # TODO: color coding by campus
--- web/static/js/edit.js
+++ web/static/js/edit.js
@@ -11,8 +11,8 @@ function addEventListenerByClass(className, event, f){
 
 
 // preventDefault for existing buttons
-addEventListenerByClass('edit-remove-li', 'click', e=>e.preventDefault())
-addEventListenerByClass('edit-add-li', 'click', e=>e.preventDefault())
+addEventListenerByClass('edit-remove-li', 'click', e=>e.preventDefault());
+addEventListenerByClass('edit-add-li', 'click', e=>e.preventDefault());
 
 // attach handler to parent node
 addEventListenerByClass('edit-dynamic-ul', 'click', e=>{
@@ -26,4 +26,74 @@ addEventListenerByClass('edit-dynamic-ul', 'click', e=>{
                             '<input type="text" name="features" value=""/>'
         ul.insertBefore(newLi, button.parentNode);
     }
+});
+
+
+// fetch options from backend and insert them as children of <select>
+function fetchInsertOptions(selectDom, prevSelections, selectedOption=null) {
+    let hdr = new Headers();
+    hdr.append('Content-Type', 'application/json');
+    let req = new Request("/fetch/edit/cat", {
+        method: 'POST',
+        body: JSON.stringify(prevSelections),
+        headers: hdr
+    });
+    fetch(req)
+    .then(response => response.json())
+    .then(listOfOptions => {
+        selectDom.innerHTML = "";
+        listOfOptions.map(option => {
+            var optionDom = document.createElement('option');
+            optionDom.setAttribute('value', option);
+            optionDom.innerHTML = option;
+            if (option==selectedOption) optionDom.selected = true;
+            selectDom.appendChild(optionDom);
+        });
+    })
+}
+
+
+
+
+// initialize options, run once each refresh
+var editCat = document.getElementById('edit-cat');
+var editBucket = document.getElementById('edit-bucket');
+var editItem = document.getElementById('edit-item');
+(function (){
+    var selectedCat = editCat.getAttribute('data-selected');
+    var selectedBucket = editBucket.getAttribute('data-selected');
+    var selectedItem = editItem.getAttribute('data-selected');
+    
+    for (child of editCat.children) {
+        if (child.value == selectedCat) child.selected=true;
+    };
+
+    fetchInsertOptions(editBucket, {'cat':selectedCat}, selectedBucket);
+    fetchInsertOptions(editItem, {'cat':selectedCat, 'bucket': selectedBucket}, selectedItem);
+})();
+
+
+// detect change in selections
+addEventListenerByClass('edit-cat-select', 'change', e=>{
+    var curCat = editCat.options[editCat.selectedIndex].value;
+    if (e.target.id == "edit-cat") {
+        fetchInsertOptions(editBucket, {'cat':curCat});
+        editItem.innerHTML = "";
+    } else if (e.target.id == "edit-bucket") {
+        var curBucket = editBucket.options[editBucket.selectedIndex].value;
+        fetchInsertOptions(editItem, {'cat':curCat, 'bucket': curBucket});
+    }
 })
+
+
+// auto update google map
+// var timeout = null;
+// var editLocation = document.getElementById('edit-location');
+// var mapApi = document.getElementById('edit-location-api');
+// editLocation.addEventListener('keyup', e => {
+//     clearTimeout(timeout);
+//     timeout = setTimeout(function () {
+//         mapApi.src;
+//     }, 1000);
+// });
+
